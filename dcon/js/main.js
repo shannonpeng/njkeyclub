0d3d8019a9ff3671377648277aa5eb662db14090
Shannon Peng <shannonpeng98@gmail.com>
2016-01-29 23:04:28 -0500

    Finish dcon 2016 home page

--- dcon/js/main.js
+++ dcon/js/main.js
@@ -1,3 +1,19 @@
-$("nav li.logo").click(function(){
-	$("nav li").css("display", "block");
+var navBarOpen = false;
+
+$(window).load(function() {
+	$("nav li.logo").click(function(){
+		if (navBarOpen) {
+			$("nav li").css("display", "none");
+			$("nav li.logo").css("display", "block");
+		}
+		else {
+			$("nav li").css("display", "block");
+		}
+		navBarOpen = !navBarOpen;
+	});
 });
+
+$(window).resize(function(){
+	$("nav li").removeAttr('style');
+});
+
