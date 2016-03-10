4acf29cf8d9f5aa7e7a984411cc141ff2245f5c8
Shannon Peng <shannonpeng98@gmail.com>
2016-03-09 22:34:13 -0500

    Create spirit stick poll

--- dcon-2016/live/MainController.js
+++ dcon-2016/live/MainController.js
@@ -4,10 +4,43 @@ app.controller('MainController', function($scope, $firebaseArray) {
 	var friday = new Firebase("//njdcon2016.firebaseio.com/schedule/friday");
 	var saturday = new Firebase("//njdcon2016.firebaseio.com/schedule/saturday");
 	var sunday = new Firebase("//njdcon2016.firebaseio.com/schedule/sunday");
+	var voted = false;
 	$scope.poll = $firebaseArray(poll);
 
 	$scope.friday = $firebaseArray(friday);
 	$scope.saturday = $firebaseArray(saturday);
 	$scope.sunday = $firebaseArray(sunday);
-	
+
+	$scope.loadPoll = function() {
+		$scope.$apply(function() {
+			$scope.poll.sort(function(a, b) { return b.votes - a.votes});
+		})
+		$("#spirit .results div").css("width", "200px");
+		var totalVotes = 0;
+		for (var i = 0; i < $scope.poll.length; i++) {
+			totalVotes += $scope.poll[i].votes;
+		}
+		for (var i = 0; i < $scope.poll.length; i++) {
+			var division = $scope.poll[i];
+			$("#poll-" + division.number).animate({"width": 200 + division.votes / totalVotes * 1000 + "px"});
+		}
+	};
+
+	$scope.vote = function(n) {
+		if (voted)
+			return false;
+		for (var i = 0; i < $scope.poll.length; i++) {
+			if ($scope.poll[i].number == n) {
+				$scope.$apply(function() {
+					var item = $scope.poll.$getRecord($scope.poll.$keyAt(i));
+					item.votes++;
+					$scope.poll.$save(item);
+				});
+				break;
+			}
+		}
+		voted = true;
+		return true;
+	}
+
 });
--- dcon-2016/live/app.js
+++ dcon-2016/live/app.js
@@ -1,5 +1,18 @@
 var app = angular.module("app", ["firebase"]);
 
 $(window).load(function() {
+	updatePoll();
 });
+
+var updatePoll = function() {
+	angular.element($('.main')).scope().loadPoll();
+	setTimeout(updatePoll, 30000);
+}
+
+var vote = function(e) {
+	var id = String(e.id);
+	if (angular.element($('.main')).scope().vote(parseInt(id.substring(5, id.length)))) {
+		$(e).addClass("voted");
+	}
+};
 
